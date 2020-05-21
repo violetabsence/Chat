5a987a8b5b454132dbbe89f179897596cc31a287
violetabsence <violetabsence@gmail.com>
2020-05-21 14:41:14 +0700

    Import icons Font Awesome in App.tsx

--- Chat/ClientApp/src/App.tsx
+++ Chat/ClientApp/src/App.tsx
@@ -5,8 +5,12 @@ import { Home } from "./components/Home";
 import { ChatWindow } from "./components/ChatWindow";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-import "./custom.css"
+import "./custom.css";
+
+library.add(fas);
 
 export default class App extends Component {
     static displayName = App.name;
