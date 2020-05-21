975b978fd2d0a507304afe1d556600766016f72f
violetabsence <violetabsence@gmail.com>
2020-05-21 14:58:19 +0700

    Remove unused components.

--- Chat/ClientApp/src/App.tsx
+++ Chat/ClientApp/src/App.tsx
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router";
 import { Layout } from "./components/Layout";
-import { Home } from "./components/Home";
 import { ChatWindow } from "./components/ChatWindow";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
@@ -18,10 +15,7 @@ export default class App extends Component {
     render() {
         return (
             <Layout>
-                <Route exact path="/" component={Home} />
-                <Route path="/counter" component={Counter} />
-                <Route path="/chat" component={ChatWindow} />
-                <Route path="/fetch-data" component={FetchData} />
+                <Route exact path="/" component={ChatWindow} />
             </Layout>
         );
     }
