2fb55296c49277442dd0c8d9b167b37d170a3e04
violetabsence <violetabsence@gmail.com>
2020-05-22 18:19:10 +0700

    Use UserService in ChatPage.

--- Chat/ClientApp/src/components/ChatPage.tsx
+++ Chat/ClientApp/src/components/ChatPage.tsx
@@ -7,49 +7,35 @@ import { Message } from "./Message";
 import { OnlineStatus } from "../models/OnlineStatus";
 import { Contact } from "./Contact";
 import { RouteComponentProps } from "react-router";
+import { UserService } from "../services/UserService";
 
-export class ChatPage extends Component<RouteComponentProps, { activeUserId?: number }> {
+interface State {
+    activeUserId?: number;
+    currentUser?: UserDto;
+    contacts?: UserDto[];
+}
+
+export class ChatPage extends Component<RouteComponentProps, State> {
     static displayName = ChatPage.name;
 
     constructor(props: RouteComponentProps) {
         super(props);
         this.state = {
-            activeUserId: undefined
+            activeUserId: undefined,
+            currentUser: undefined,
+            contacts: undefined
         };
     }
 
-    setActiveUserId(activeUserId?: number): void {
-        this.setState({ activeUserId });
-    }
-
-    getUsers(): UserDto[] {
-        return [
-            { id: 0, img: "http://emilcarlsson.se/assets/mikeross.png", username: "Mike Ross", status: OnlineStatus.online },
-            { id: 1, img: "http://emilcarlsson.se/assets/harveyspecter.png", username: "Harvey Specter", status: OnlineStatus.busy },
-            { id: 2, img: "http://emilcarlsson.se/assets/rachelzane.png", username: "Rachel Zane", status: OnlineStatus.away },
-            { id: 3, img: "http://emilcarlsson.se/assets/donnapaulsen.png", username: "Donna Paulsen", status: OnlineStatus.online },
-            { id: 4, img: "http://emilcarlsson.se/assets/jessicapearson.png", username: "Jessica Pearson", status: OnlineStatus.busy },
-            { id: 5, img: "http://emilcarlsson.se/assets/haroldgunderson.png", username: "Harold Gunderson", status: OnlineStatus.online },
-            { id: 6, img: "http://emilcarlsson.se/assets/danielhardman.png", username: "Daniel Hardman", status: OnlineStatus.online },
-            { id: 7, img: "http://emilcarlsson.se/assets/katrinabennett.png", username: "Katrina Bennett", status: OnlineStatus.busy },
-            { id: 8, img: "http://emilcarlsson.se/assets/charlesforstman.png", username: "Charles Forstman", status: OnlineStatus.online },
-            { id: 9, img: "http://emilcarlsson.se/assets/jonathansidwell.png", username: "Jonathan Sidwell", status: OnlineStatus.offline }
-        ];
-    }
-
-    getContacts(userId: number): UserDto[] {
-        return this.getUsers().filter(u => u.id !== userId);
-    }
-
-    getUser(id: number): UserDto {
-        
-
-        const user = this.getUsers().find(u => u.id === id);
-        if (user === undefined) {
-            throw new Error;
-        }
-
-        return user;
+    componentDidMount() {
+        UserService.contacts(0).then(
+            users => {
+                this.setState({ contacts: users });
+            },
+            error => {
+                console.error(error);
+            }
+        );
     }
 
     getMessages(): MessageDto[] {
@@ -81,8 +67,8 @@ export class ChatPage extends Component<RouteComponentProps, { activeUserId?: nu
                     </div>
                     <div id="contacts">
                         <ul>
-                            {this.getContacts(0).map(u => {
-                                return <Contact key={u.id} contact={u} isActive={this.state.activeUserId === u.id} onClick={() => this.setActiveUserId(u.id)} />
+                            {this.state.contacts !== undefined && this.state.contacts.map(u => {
+                                return <Contact key={u.id} contact={u} isActive={this.state.activeUserId === u.id} onClick={() => this.setState({ activeUserId: u.id })} />
                             })}
                         </ul>
                     </div>
